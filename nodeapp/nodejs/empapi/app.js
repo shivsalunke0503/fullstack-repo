fb65028c4ffce133bf9ce3a9012d156c4aece289
shivsalunke0503 <61627668+shivsalunke0503@users.noreply.github.com>
2021-01-24 20:14:58 +0530

    Nodeapp
    use run npm install to update node_modules

---
+++ nodeapp/nodejs/empapi/app.js
@@ -0,0 +1,62 @@
+const express 						= require ('express');
+const app 							= express();
+
+const morgan 						= require('morgan');// morgan call next function if problem occure
+const bodyParser 					= require('body-parser');// this package use to formate json data
+const mongoose 						= require ('mongoose');
+const dbname 						= 'empdb';
+
+mongoose.connect('mongodb://localhost/'+dbname,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.promise = global.Promise;
+
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+
+// const EmployeeRoutes	= require("./routes/employee.js"); 	
+// app.use("/api/employee",EmployeeRoutes);
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.json({"message":"Welcome to EmpApi Project"})
+})
+
+
+app.use((req, res, next) => {
+    const error = new Error("This URL Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+            error: {
+                message: error.message
+            }
+        });
+});
+
+module.exports = app;
---
+++ nodeapp/nodejs/empapi/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app.js'); // app file include
+
+const port  = 3003;
+console.log('port => ',port);
+
+const server = http.createServer(app);
+server.listen(port);
