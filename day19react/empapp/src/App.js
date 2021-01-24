e81d6682bc889d5095101c37d8bd0afd1d56a7bf
shivsalunke0503 <61627668+shivsalunke0503@users.noreply.github.com>
2021-01-24 20:02:08 +0530

    Day 19 React
     run command to update node_modeles npm install

---
+++ day19react/empapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import Block from './Components/FirstComponenet/FirstComponent.js';
+
+
+import './App.css';
+// import SecondComponent from './Components/SecondComponent/SecondComponent.js';
+import EmpList from './Components/EmpList/EmpList.js';
+function App() {
+  return (
+    <div className="App">
+      
+      <EmpList/>
+
+    </div>
+  );
+}
+
+export default App;
